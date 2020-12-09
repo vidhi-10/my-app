7829fc556a6443cb46f3c1158a096e234343ad73
vidhi Srivastava <vidhi.srivastava8@gmail.com>
2020-12-09 16:54:59 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListingsComponent } from './listings/listings.component';
+import { ContactComponent } from './contact/contact.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListingsComponent,
+    ContactComponent,
+    TestimonialsComponent,
+    AboutComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
