7598b20939745947095633423360ad47e3b4fe14
vidhi Srivastava <vidhi.srivastava8@gmail.com>
2020-12-09 17:35:10 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ListingsComponent } from './listings/listings.component';
+import { ListingsComponent } from './listing/listings.component';
 import { ContactComponent } from './contact/contact.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { AboutComponent } from './about/about.component';
